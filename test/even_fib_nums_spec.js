a44a667c219338bf46ba64ccb7b217c76b8f8f75
Ray Farias <ray@rayfarias.com>
2015-04-29 14:03:46 -1000

    switch from jasmine to mocha/chai; change assertion style to "should"

--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -1,18 +1,20 @@
-var challenge_2 = require("../challenge_2");
- 
+var challenge_2 = require('../challenge_2');
+var chai = require('chai');
+var should = chai.should;
+
 describe("Sum of even numbers in the Fibonnacci sequence", function () {
-  
+
   it("should return the sum of all even Fibonacci numbers when given 10", function () {
     var sum = challenge_2.sumOfEvenFibonacciNumbers(10);
-    expect(sum).toBe(10);
+    sum.should.equal(10);
   });
 
   // when your tests pass, uncomment the next test and
   // change the expected value to the correct result
-  
+
   // it("should return the sum of all even Fibonacci numbers when given 4,000,000", function () {
   //   var sum = challenge_2.sumOfEvenFibonacciNumbers(4000000);
-  //   expect(sum).toBe(X); // CHANGE THE VALUE OF X to the correct number
+  //   sum.should.equal('X'); // CHANGE THE VALUE OF 'X' to the correct number
   // });
 
-});    
+});
