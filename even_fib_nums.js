bde6e7901f359b9f90773a7b7b9af1fdc0721c75
Ray Farias <ray@rayfarias.com>
2015-05-01 11:11:57 -1000

    update copy

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,6 +1,13 @@
-exports.sumOfEvenFibonacciNumbers = function(n){
+/**
+ * Return the total sum of all even fibonacci numbers up to and including
+ * the value given to you at `maxFibValue`
+ *
+ * @param  {Number} maxFibValue
+ * @return {Number} sum
+ */
+exports.sumOfEvenFibonacciNumbers = function(maxFibValue){
   var sum = 0;
   // do your work here
-  
+
   return sum;
 };
