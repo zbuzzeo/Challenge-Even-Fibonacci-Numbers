32d4504274f83e28d8c2d7d9fc7234259e7eea7e
Jon Borgonia <jon@gomagames.com>
2015-06-30 06:54:36 -1000

    uncomment -> unskip

--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -14,15 +14,14 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
     sum.should.equal( 44 );
   });
 
-  // when your tests pass, uncomment the next test and
+  // when the 2 tests pass, unskip the next test and
   // change the expected value to the correct result
+  it.skip( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
+    var sum = fibonnaciNums(4000000);
 
-  // it( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
-  //   var sum = fibonnaciNums(4000000);
-
-  //   // CHANGE THE VALUE OF 'X' to the correct number
-  //   sum.should.equal( 'X' );
-  // });
+    // CHANGE THE VALUE OF 'X' to the correct number
+    sum.should.equal( 'X' );
+  });
 });
 
 /**
@@ -33,13 +32,13 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
  * less than the given input
  */
 
-// uncomment the following test to test your highestFibonacciNumber() function
-// describe( 'Highest number in Fibonacci sequence', function() {
-//   it( 'should return the highest Fibonacci number less than the given input', function() {
-//     var highest = fibonnaciNums.highestFibonacciNumber( 100 );
-//     highest.should.equal( 89 );
-//   });
-// });
+// unskip the following test to test your highestFibonacciNumber() function
+describe.skip( 'Highest number in Fibonacci sequence', function() {
+  it( 'should return the highest Fibonacci number less than the given input', function() {
+    var highest = fibonnaciNums.highestFibonacciNumber( 100 );
+    highest.should.equal( 89 );
+  });
+});
 
 
 //STRETCH GOALS: Define another way you could write a function for a fibonacci number and write passing tests
